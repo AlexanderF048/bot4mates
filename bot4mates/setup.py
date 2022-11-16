872833eae93de54b786d50fc84502a50a64f6141
don-ar <artem.donets.python@gmail.com>
2022-11-16 23:39:08 +0200

    setup.py updated a little bit

--- bot4mates/setup.py
+++ bot4mates/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='bot_assistant',
@@ -8,6 +8,6 @@ setup(
     author='Bot4mates',
     author_email='https://don-ar.github.io/',
     license='MIT',
-    packages=['bot4mates'],
+    packages=find_namespace_packages(),
     entry_points={'console_scripts': ['bot_assist = bot4mates.main:main']}
 )
